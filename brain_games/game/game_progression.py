6af9ef15a430d11322b4acf87939c90d0b2d4c9c
dim4ick <romanoff_d@mail.ru>
2022-12-15 01:58:09 +0300

    changes (progression)

--- brain_games/game/game_progression.py
+++ brain_games/game/game_progression.py
@@ -5,12 +5,16 @@ game = 'What number is missing in the progression?'
 
 def function():
     length = randint(5, 10)
-    start = randint(1, 5)
+    start = randint(10, 30)
     step = randint(2, 5)
     progression = list(range(start, start + length * step, step))
 
     answer = choice(progression)
-    question = ' '.join(
-        '..' if number == answer else str(number) for number in progression
-    )
+    question = progression
+    for i in range(len(question)):
+        question[i] = str(question[i])
+
+    question = ' '.join(question)
+    question = question.replace(str(answer), '..')
+
     return str(answer), question
