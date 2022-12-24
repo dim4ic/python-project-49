397c8c829003441316e0647e048b96d6379fc84a
dim4ick <romanoff_d@mail.ru>
2022-12-24 19:54:05 +0300

    update "PRIME"

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,18 +3,23 @@ from random import randint
 THE_TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def make_game():
-
-    question = randint(2, 30)
+def is_prime(question):
     count = 2
 
-    while count <= question - 1:
+    while count <= question // 2:
 
         if question % count == 0:
-            correct_answer = 'no'
+
+            return True
             break
         count += 1
 
+
+def make_game():
+
+    question = randint(2, 30)
+    if is_prime(question):
+        correct_answer = 'no'
     else:
         correct_answer = 'yes'
 
