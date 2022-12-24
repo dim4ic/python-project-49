ef07b66ae481a58efb4023f4fcac7607309182d5
dim4ick <romanoff_d@mail.ru>
2022-12-24 10:45:16 +0300

    apdate "PROGRESSION"

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,18 +3,25 @@ from random import randint, choice
 THE_TASK = 'What number is missing in the progression?'
 
 
+def progression(start, length, step):
+    return list(range(start, start + length * step, step))
+
+
 def make_game():
     length = randint(5, 10)
-    start = randint(10, 30)
+    start = randint(1, 5)
     step = randint(2, 5)
-    progression = list(range(start, start + length * step, step))
+    question = progression(start, length, step)
 
-    correct_answer = choice(progression)
-    question = progression
-    for i in range(len(question)):
-        question[i] = str(question[i])
+    correct_answer = choice(question)
+    index = 0
+    for number in question:
+        if number == correct_answer:
+            question[index] = '..'
+        else:
+            str(number)
+            index += 1
 
-    question = ' '.join(question)
-    question = question.replace(str(correct_answer), '..')
+    question = ' '.join(str(i) for i in question)
 
     return str(correct_answer), question
